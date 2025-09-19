e493853313c4814f12f3acba4c7cc8764ae6d567
HarryCMather <harrycmather@gmail.com>
2025-09-19 21:03:08 +0100

    feat: Add newly created stored procedures to initial migration

--- src/DQRetro.TournamentTracker.Db/Migrations/Script0001 - Initial Table and SP Creation.sql
+++ src/DQRetro.TournamentTracker.Db/Migrations/Script0001 - Initial Table and SP Creation.sql
@@ -1038,6 +1038,109 @@ GRANT EXECUTE
     ON OBJECT::[dbo].[UpdateExistingUsers] TO [TournamentTracker]
     AS [dbo];
 GO
+
+--      CheckIfEventVideoExists:
+CREATE PROCEDURE [dbo].[CheckIfEventVideoExists]
+    @YouTubeVideoId VARCHAR(16)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT TOP 1 1
+      FROM dbo.EventVideo
+     WHERE [YouTubeVideoId] = @YouTubeVideoId;
+END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[CheckIfEventVideoExists] TO [TournamentTracker]
+    AS [dbo];
+GO
+
+--      InsertEventVideo:
+CREATE PROCEDURE InsertEventVideo
+    @YouTubeChannelId VARCHAR(32),
+    @YouTubeChannelName NVARCHAR(64),
+    @EventId SMALLINT,
+    @Title NVARCHAR(128),
+    @YouTubeVideoId VARCHAR(16),
+    @YouTubeVideoUrl VARCHAR(256),
+    @YouTubeVideoThumbnailUrl VARCHAR(256),
+    @ReleaseDate DATETIME
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Retrieve the VideoChannelId if the channel already exists,
+    -- or create it if it's new:
+    DECLARE @VideoChannelId SMALLINT;
+
+    SELECT @VideoChannelId = [Id]
+      FROM dbo.VideoChannel
+     WHERE [YouTubeChannelId] = @YouTubeChannelId
+       AND [Name] = @YouTubeChannelName;
+
+    IF @VideoChannelId IS NULL
+        BEGIN
+            INSERT INTO dbo.VideoChannel ([YouTubeChannelId], [Name])
+            VALUES (@YouTubeChannelId, @YouTubeChannelName);
+
+            SELECT @VideoChannelId = [Id]
+              FROM dbo.VideoChannel
+             WHERE [YouTubeChannelId] = @YouTubeChannelId
+               AND [Name] = @YouTubeChannelName;
+        END
+
+    -- Everything should now be ready for inserting the EventVideo:
+    INSERT INTO dbo.EventVideo ([VideoChannelId], [EventId], [Title], [YouTubeVideoId], [YouTubeVideoUrl], [YouTubeThumbnailUrl], [ReleaseDate])
+    VALUES (@VideoChannelId, @EventId, @Title, @YouTubeVideoId, @YouTubeVideoUrl, @YouTubeVideoThumbnailUrl, @ReleaseDate);
+END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[InsertEventVideo] TO [TournamentTracker]
+    AS [dbo];
+GO
+
+--      GetEventVideos:
+CREATE PROCEDURE GetEventVideos
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT ev.[Title]
+         , ev.[YouTubeVideoId]
+         , ev.[YouTubeVideoUrl]
+         , ev.[YouTubeThumbnailUrl]
+         , ev.[ReleaseDate]
+         , vc.[Name] AS [ChannelName]
+         , vc.[YouTubeChannelId] AS [YouTubeChannelId]
+         , e.[Name] AS [EventName]
+      FROM dbo.EventVideo ev
+      LEFT JOIN dbo.VideoChannel vc
+        ON ev.[VideoChannelId] = vc.[Id]
+      LEFT JOIN dbo.[Event] e
+        ON ev.[EventId] = e.[Id];
+END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[GetEventVideos] TO [TournamentTracker]
+    AS [dbo];
+GO
+
+--      GetYouTubeChannelIds:
+CREATE PROCEDURE GetYouTubeChannelIds
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT [YouTubeChannelId]
+      FROM dbo.[VideoChannel];
+END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[GetYouTubeChannelIds] TO [TournamentTracker]
+    AS [dbo];
+GO
+
 -- End of creating required stored procedures.
 
 
