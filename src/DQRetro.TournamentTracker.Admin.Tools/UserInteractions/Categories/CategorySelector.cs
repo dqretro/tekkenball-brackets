ac33fe13bcfcf8f3a3768dca87a523e9dd2b83cd
HarryCMather <harrycmather@gmail.com>
2025-09-29 21:32:07 +0100

    feat: Create CategorySelector, ICategoryItem and ICategoryItemSelector

---
+++ src/DQRetro.TournamentTracker.Admin.Tools/UserInteractions/Categories/CategorySelector.cs
@@ -0,0 +1,49 @@
+﻿namespace DQRetro.TournamentTracker.Admin.Tools.UserInteractions.Categories;
+
+/// <summary>
+/// Selects a given category, allowing category items to be selected and executed.
+/// </summary>
+public sealed class CategorySelector
+{
+    private readonly IReadOnlyDictionary<byte, ICategoryItemSelector> _categories;
+    private readonly List<string> _categoryDescriptions;
+    private readonly byte _exitOption;
+
+    /// <summary>
+    /// Ctor.
+    /// </summary>
+    /// <param name="categories"></param>
+    public CategorySelector(IReadOnlyDictionary<byte, ICategoryItemSelector> categories)
+    {
+        _categories = categories;
+        _categoryDescriptions = _categories.Select(kvp => $"{kvp.Key} - {kvp.Value.Description}").ToList();
+        _exitOption = (byte)(_categories.Count + 1);
+        _categoryDescriptions.Add($"{_exitOption} - Go Back");
+    }
+
+    /// <summary>
+    /// Selects and executes a category from the built-up and provided list.
+    /// </summary>
+    public async Task SelectCategoryAsync()
+    {
+        while (true)
+        {
+            Console.WriteLine("Available Categories:\n");
+            Console.WriteLine(string.Join('\n', _categoryDescriptions));
+
+            string userInput = Console.ReadLine();
+            if (!byte.TryParse(userInput, out byte userInputByte) || userInputByte < 1 || userInputByte > _exitOption)
+            {
+                Console.WriteLine("Error: Input could not be parsed. Try again.");
+                continue;
+            }
+            else if (userInputByte == _exitOption)
+            {
+                return;
+            }
+
+            ICategoryItemSelector categoryItemSelector = _categories[userInputByte];
+            await categoryItemSelector.SelectAndExecuteCategoryItemAsync();
+        }
+    }
+}
---
+++ src/DQRetro.TournamentTracker.Admin.Tools/UserInteractions/Categories/ICategoryItem.cs
@@ -0,0 +1,18 @@
+﻿namespace DQRetro.TournamentTracker.Admin.Tools.UserInteractions.Categories;
+
+/// <summary>
+/// Represents a given Category Item within a Category.
+/// </summary>
+public interface ICategoryItem
+{
+    /// <summary>
+    /// The description to be displayed to the user for a given action.
+    /// </summary>
+    string Description { get; }
+
+    /// <summary>
+    /// Executes the selected CategoryItem.
+    /// </summary>
+    /// <returns></returns>
+    Task ExecuteAsync();
+}
