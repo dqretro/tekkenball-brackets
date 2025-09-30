1a22271c765eae26ffe71bf129ab0b8b677dce83
HarryCMather <94319888+HarryCMather@users.noreply.github.com>
2025-09-30 12:23:14 +0100

    chore: EventVideo Comment in AdminTools

--- src/DQRetro.TournamentTracker.Admin.Tools/Models/EventVideo.cs
+++ src/DQRetro.TournamentTracker.Admin.Tools/Models/EventVideo.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Model representing this API's GetVideos endpoint response.
-/// Pretty much a copy of the API's model.
+/// A copy of the APIs EventVideo model code, to avoid importing the API as a dependency, or creating separate modules.
 /// </summary>
 public sealed class EventVideo
 {
