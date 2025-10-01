f477a6ca6e8e9ef7fbc094821560582602cc8bdb
HarryCMather <harrycmather@gmail.com>
2025-10-01 19:23:46 +0100

    feat: Allow multiple admin-tools-cli video exclusions in batches

--- src/DQRetro.TournamentTracker.Admin.Tools/UserInteractions/Categories/Videos/CategoryItems/ExcludeVideoCategoryItem.cs
+++ src/DQRetro.TournamentTracker.Admin.Tools/UserInteractions/Categories/Videos/CategoryItems/ExcludeVideoCategoryItem.cs
@@ -1,4 +1,5 @@
-﻿using DQRetro.TournamentTracker.Admin.Tools.Models;
+﻿using System.Text.RegularExpressions;
+using DQRetro.TournamentTracker.Admin.Tools.Models;
 using DQRetro.TournamentTracker.Admin.Tools.Persistence;
 
 namespace DQRetro.TournamentTracker.Admin.Tools.UserInteractions.Categories.Videos.CategoryItems;
@@ -8,6 +9,9 @@ namespace DQRetro.TournamentTracker.Admin.Tools.UserInteractions.Categories.Vide
 /// </summary>
 public sealed class ExcludeVideoCategoryItem : ICategoryItem
 {
+    private static readonly Regex CsvRegex = new(@"^\d+(,\d+)*$", RegexOptions.Compiled);
+    private static readonly Regex NumberRangeRegex = new(@"^\d+-\d+$", RegexOptions.Compiled);
+
     private readonly VideoAdminToolsSqlRepository _videoAdminToolsSqlRepository;
 
     /// <summary>
@@ -34,27 +38,89 @@ public sealed class ExcludeVideoCategoryItem : ICategoryItem
             return;
         }
 
-        Console.WriteLine("Enter the number to the left of the video you wish to exclude:");
+        List<ushort> videoIndexes = GetVideoIndexesToExclude(videos);
+        string videoIdsCsv = GetVideoIdsCsvToExclude(videos, videoIndexes);
+        await _videoAdminToolsSqlRepository.ExcludeVideosAsync(videoIdsCsv);
+
+        Console.WriteLine("Videos successfully excluded...");
+    }
+
+    private static List<ushort> GetVideoIndexesToExclude(List<EventVideo> videos)
+    {
+        Console.WriteLine("Enter the numbers to the left of the video you wish to exclude:\n" +
+                          "Valid inputs include the following types, and should not include spaces.\n" +
+                          "It's currently only possible to perform one range at a time, and 1 input type at a time:\n" +
+                          "\tNumber Ranges (ie: 1-3 which would select videos 1, 2, and 3 for exclusion)\n" +
+                          "\tNumbers CSV (ie: 4,5,6 which would select videos 4, 5, and 6 for exclusion)\n" +
+                          "\tSingle numbers (ie: 7 which would select video 7 for exclusion):");
         for (ushort count = 1; count <= videos.Count; count++)
         {
             Console.WriteLine($"{count}\t\"{videos[count - 1].Title}\"\tBy \"{videos[count - 1].ChannelName}\"");
         }
 
-        ushort videoIndex;
+        List<ushort> videoIndexes = [];
+
         while (true)
         {
             string userInput = Console.ReadLine();
-            if (ushort.TryParse(userInput, out videoIndex) || videoIndex < 1 || videoIndex > videos.Count)
+            if (string.IsNullOrEmpty(userInput))
             {
-                // Return to 0-based indexing:
-                videoIndex--;
+                Console.WriteLine("Error: Input could not be empty. Try again.");
+            }
+            else if (IsValidCsvInput(userInput))
+            {
+                IEnumerable<ushort> numbers = userInput.Split(',')
+                                                       .Select(num => ushort.Parse(num));
+
+                videoIndexes.AddRange(numbers);
+            }
+            else if (IsValidNumberRangeInput(userInput))
+            {
+                ushort[] numbers = userInput.Split('-')
+                                            .Select(num => ushort.Parse(num))
+                                            .OrderBy(num => num)
+                                            .ToArray();
+
+                IEnumerable<ushort> numberRanges = Enumerable.Range(numbers[0], numbers[1] - numbers[0] + 1)
+                                                             .Select(num => (ushort)num);
+
+                videoIndexes.AddRange(numberRanges);
+            }
+            else if (ushort.TryParse(userInput, out ushort videoIndex))
+            {
+                videoIndexes.Add(videoIndex);
+            }
+
+            if (IsValidVideoIndexes(videos, videoIndexes))
+            {
                 break;
             }
 
             Console.WriteLine("Error: Input could not be parsed. Try again.");
         }
 
-        Console.WriteLine($"Excluding Video \"{videos[videoIndex].Title}\" by \"{videos[videoIndex].ChannelName}\"");
-        await _videoAdminToolsSqlRepository.ExcludeVideoAsync(videos[videoIndex].YouTubeVideoId);
+        return videoIndexes;
+    }
+
+    private static bool IsValidCsvInput(string csvInput)
+    {
+        return CsvRegex.IsMatch(csvInput);
+    }
+
+    private static bool IsValidNumberRangeInput(string numberRangeInput)
+    {
+        return NumberRangeRegex.IsMatch(numberRangeInput);
+    }
+
+    private static bool IsValidVideoIndexes(List<EventVideo> videos, List<ushort> videoIndexes)
+    {
+        return videoIndexes.Count > 0 && videoIndexes.TrueForAll(num => num >= 1 && num <= videos.Count);
+    }
+
+    private static string GetVideoIdsCsvToExclude(List<EventVideo> videos, List<ushort> videoIndexes)
+    {
+        // videoIndexes are the "more friendly" 1-based indexing, so convert to 0-based indexing before converting to csv:
+        IEnumerable<string> videoIds = videoIndexes.Select(num => videos[num - 1].YouTubeVideoId);
+        return string.Join(',', videoIds);
     }
 }
